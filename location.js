ef0cfede3c43a1a209bb79549bec11f7611e3f73
mjtBootcamp <115650100+mjtBootcamp@users.noreply.github.com>
2023-04-08 08:42:46 -0400

    Add files via upload

--- location.js
+++ location.js
@@ -1,12 +1,10 @@
 const comunas = require("./data/comunas.json");
-const comunaCercana = (comunas, lat, long) => {
+const comunaCercana = (lat, long, tolerancia = 5) => {
     
-    let latCateto = 0;
-    let longCateto = 0;
     //Teorema de pitágoras
     comunas.map((comuna) => {
-        latCateto = comuna.lat - lat;
-        longCateto = comuna.lng - long;
+        let latCateto = comuna.lat - lat;
+        let longCateto = comuna.lng - long;
         //111 kilometros por grado
         comuna.dist = 111 * Math.sqrt(latCateto ** 2 + longCateto ** 2); //kilometros
     });
@@ -15,15 +13,9 @@ const comunaCercana = (comunas, lat, long) => {
         if (a.dist < b.dist) return -1;
         return 0;
     });
-    let diferencia = comunas[0].dist - comunas[1].dist;
-    
+    let diferencia = comunas[1].dist - comunas[0].dist;
+        
     //Conurbaciones
-    let tolerancia = 5;
     let resultado = diferencia > tolerancia ? comunas[0] : [comunas[0], comunas[1]];
     return resultado;
 };
-//let resultado= comunaCercana(comunas,-29.950179, -71.273715);
-//console.log(resultado)
-module.exports = {
-    comunaCercana, comunas
-}
